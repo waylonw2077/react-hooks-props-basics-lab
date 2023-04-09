4a6f329ad6757adc57b57515a1403cd3a154d83d
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-04-09 19:24:17 -0400

    completes assignment

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+
+function Links(props) {
+    const { github, linkedin } = props;
+  
+    return (
+      <div>
+        <h3>Links</h3>
+        <a href={github}>{github}</a>
+        <a href={linkedin}>{linkedin}</a>
+      </div>
+    );
+  }
+  
+  export default Links;
